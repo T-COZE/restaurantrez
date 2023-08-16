048d22ea44ba63016d7dd31238c66ee122003140
Thomas Styron <thomasstyron@gmail.com>
2023-08-16 10:29:20 -0500

    new repo

---
+++ back-end/src/reservations/reservations.controller.js
@@ -0,0 +1,213 @@
+const service = require("./reservations.service")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+const date = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+const time = /[0-9]{2}:[0-9]{2}/;
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]
+
+const validProps =[
+  "first_name", "last_name","mobile_number","people","reservation_date","reservation_time"
+]
+
+const statuses = ["booked", "canceled", "finished", "seated"]
+
+
+async function list(req, res) {
+  // get the date, req.query.date
+  const {date, mobile_number}= req.query
+  let reservations
+  if(date){
+    reservations = await service.listByDate(date)
+  } else if (mobile_number) {
+    reservations = await service.listByNumber(mobile_number)
+  }
+  res.json({data: reservations})
+}
+async function create(req, res){
+  const reservation = await service.create(req.body.data)
+  res.status(201).json({data: reservation})
+}
+
+function hasValidProps(req, res, next){
+  const {data= {}} = req.body
+  if(!data){
+    return next({
+      status: 400, message: "Requres data"
+    })
+  }
+  validProps.forEach((property) => {
+    if (!data[property]) {
+      return next({
+        status: 400,
+        message: `requires ${property}`,
+      });
+    }
+
+    if (property === "people" && !Number.isInteger(data.people)) {
+      return next({
+        status: 400,
+        message: `requires ${property} to be a number`,
+      });
+    }
+
+    if (
+      property === "reservation_date" &&
+      !date.test(data.reservation_date)
+    ) {
+      return next({
+        status: 400,
+        message: `requires ${property} to be properly formatted as YYYY-MM-DD`,
+      });
+    }
+
+    if (
+      property === "reservation_time" &&
+      !time.test(data.reservation_time)
+    ) {
+      return next({
+        status: 400,
+        message: `requires ${property} to be properly formatted as HH:MM`,
+      });
+    }
+  });
+
+  next();
+
+}
+
+function isReserved(req, res, next){
+  const {data} = req.body
+  if(data.status === "finshed" || data.status ==="seated"){
+    return next({
+      status: 400, message: "Reservations cannot be made with seated or finished status"
+    })
+  }
+  next()
+}
+
+function isValidDay(req, res, next) {
+  const { data } = req.body;
+  const reservationDate = new Date(
+    `${data.reservation_date} ${data.reservation_time}`
+  );
+  let day = days[reservationDate.getDay()];
+  let time = data.reservation_time;
+  if (reservationDate < new Date() && day === "Tuesday") {
+    return next({
+      status: 400,
+      message:
+        "Reservations can only be created on a future date, excluding Tuesdays",
+    });
+  }
+  if (reservationDate < new Date()) {
+    return next({
+      status: 400,
+      message: "Reservations can only be created on a future date",
+    });
+  }
+  if (day === "Tuesday") {
+    return next({
+      status: 400,
+      message: "Restaurant is closed on Tuesdays",
+    });
+  }
+  if (time <= "10:30" || time >= "21:30") {
+    return next({
+      status: 400,
+      message: "Reservations can only be made from 10:30AM - 9:30PM.",
+    });
+  }
+  next();
+}
+
+async function update(req, res, next) {
+  const updated = {
+    ...req.body.data,
+    reservation_id: res.locals.reservation.reservation_id,
+  };
+
+  service.updateReservation(updated)
+    .then((data) => res.json({ data }))
+    .catch(next);
+} 
+
+async function changeStatus(req, res, next){
+  const changed = {...req.body.data,
+  reservation_id: res.locals.reservation.reservation_id
+}
+service.update(changed)
+.then((data)=>res.json({data})).catch(next)
+
+}
+
+function validStatus(req, res, next){
+  const {status} = req.body.data
+  if(statuses.includes(status)){
+   status =res.locals.status  
+   next()
+  } else {
+    next({
+      status: 400, message: "Status not accepted.  Status must be: 'booked', 'seated', or 'finished."
+    })
+  }
+}
+
+async function read(_req, res, _next){
+  res.json({data: res.locals.reservation})
+}
+
+async function reservationExists(req, res, _next){
+  const {reservation_id}= req.params
+  const reservation = await service.read(reservation_id)
+  if(reservation){
+    res.locals.reservation = reservation
+    return next()
+  }
+  next({
+    status:404, message: `Reservation ${reservation_id? reservation_id: ""} Not Found`
+  })
+}
+
+ function unfinished(req, res, next){
+  const {reservation_id} = req.params
+  const status = res.locals.reservation.status
+
+  if(status === "finished"){
+    return next({
+      status: 400, message: `This reservation ${reservation_id} is in the past.  We cannot time travel in 2035!`
+    })
+  }
+  next()
+ }
+
+
+
+module.exports = {
+  list: [asyncErrorBoundary(list)],
+  read: [asyncErrorBoundary(reservationExists), read],
+  updatae: [asyncErrorBoundary(update),
+  asyncErrorBoundary(reservationExists),
+hasValidProps,
+],
+create: [asyncErrorBoundary(create),
+hasValidProps, isValidDay,isReserved
+],
+update: [asyncErrorBoundary(reservationExists),
+hasValidProps,
+asyncErrorBoundary(update)],
+changeStatus: [asyncErrorBoundary(reservationExists),
+validStatus,
+unfinished,
+asyncErrorBoundary(changeStatus)],  
+
+};
---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,48 @@
+const knex = require("../db/connection")
+const table = "reservations"
+
+async function create(reservation){
+    return knex(table)
+    .insert(reservation).returning("").then((created)=>
+    created[0])
+}
+async function listByNumber(mobile_number){
+    return knex(table)
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date")
+}
+
+async function listByDate(reservation_date) {
+    return knex(table).select("*").where({ reservation_date }).whereNot({ status: "finished" })
+      .whereNot({ status: "cancelled" }).orderBy("reservation_time");
+  }
+
+async function read(reservation_id){
+    return knex(table).select("*").where({reservation_id}).first()
+}
+async function update(reservation){
+    return knex(table).where({reservation_id: reservation_id}).update(reservation,"*")
+    .then((updated) => updated[0])
+}
+async function newStatus(reservation_id, status){
+    return knex(table).where({reservation_id}).update({status}, "*").then((updated)=> updated[0])
+}
+async function updateReservation(updatedData) {
+    const { reservation_id, ...restOfData } = updatedData;
+  
+    return knex(table).where({ reservation_id })
+      .update(restOfData).returning('*')
+  }
+
+module.exports = {
+    create,
+    listByNumber,
+    listByDate,
+    read,
+    update,
+    updateReservation,
+    newStatus,
+}
